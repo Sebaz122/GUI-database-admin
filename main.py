720bf668b785e52df23c56b930f4c4f3fc42cb3f
Sebaz122 <szarebski@student.agh.edu.pl>
2024-12-15 21:23:35 +0100

    editing records+ addjustments on printing

--- main.py
+++ main.py
@@ -49,22 +49,89 @@ def print_table(root, cursor, table_name):
     h.pack(side=RIGHT, fill=Y)
     table_frame=Frame(canvas)
     canvas.create_window((0, 0), window=table_frame, anchor="nw")
+    column_names=fetch_table_columns(cursor, table_name)
 
-
-
+    for j in range(total_cols):
+        header = Label(table_frame, text=f"{column_names[j]}", font=('Arial', 12, 'bold'), background="lightblue")
+        header.grid(row=0, column=j, sticky="NEWS")
+        
     for i in range(total_rows):
         for j in range(total_cols):
             e = Entry(table_frame, width=20, fg='blue',
                       background="lightblue", font=('Arial', 12))
-            e.grid(row=i, column=j, sticky="NEWS")
+            e.grid(row=i+1, column=j, sticky="NEWS")
             e.insert(END, dataset[i][j])
     table_frame.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
     canvas.configure(yscrollcommand=h.set)
 
 
-    
+def edit_record(root,cursor, conn):
 
+    def create_form(selected_table):
+        for widget in top.winfo_children():
+            widget.destroy()
+        columns = fetch_table_columns(cursor, selected_table)
+        Label(top, text=f"Podaj id rekordu który chcesz edytować z tabeli: {selected_table}", font=("Arial", 12, "bold")).pack()
+        entries.clear()
+        Label(top, text=columns[0]).place(x=10,y=30)
+        entry_id=Entry(top)
+        entry_id.place(x=140,y=30)
+        button_id=Button(top, text="Wyszukaj rekord", command=lambda: clear_and_create(top, columns, entry_id.get(), selected_table))
+        button_id.place(x=10,y=70)
+
+    def clear_and_create(top, columns, entry_id, selected_table):
+        for widget in top.winfo_children():
+            widget.destroy()
+        if entry_id:
+            cursor.execute(f"SELECT * FROM {selected_table} WHERE {columns[0]} = {entry_id}")
+            record=cursor.fetchone()
+            if record:
+                for idx, column in enumerate(columns[1:]):
+                    Label(top, text=column).place(x=10, y=10 + idx * 40 + 20)
+                    entry = Entry(top)
+                    entry.place(x=140, y=10 + idx * 40 + 20)
+                    entry.insert(END, record[idx+1])
+                    entries[column] = entry
+                button_add = Button(top, text="Edytuj rekord", command= lambda: save_record(top, selected_table, columns, entry_id))
+                button_add.place(x=10, y=10 + len(columns) * 30 + 70)
+            else:
+                Label(top, text="Rekord nie został znaleziony")
+        else:
+            Label(top, text="Podaj ID rekordu")
+
+    def save_record(top, table_name, columns, record_id):
+        data = {col: entries[col].get() for col in columns[1:]}  
+        updates = ', '.join([f"{col} = %s" for col in columns[1:]]) 
+        values = list(data.values()) + [record_id] 
+        try:
+            cursor.execute(f"UPDATE {table_name} SET {updates} WHERE {columns[0]} = %s;", values)
+            conn.commit()
+            Label(top, text="Rekord zaktualizowany pomyślnie!", fg="green").place(x=10, y=10 + len(columns) * 40 + 70)
+        except Exception as e:
+            Label(top, text=f"Błąd: {str(e)}", fg="red").place(x=10, y=10 + len(columns) * 40 + 70)
+    
+        
+
+    def select_table():
+        selected_table = table_name_var.get()
+        if selected_table:
+            create_form(selected_table)
+
+    top = Toplevel(root)
+    top.geometry("400x400")
+    top.attributes('-topmost')
+    cursor.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';")
+    tables = [row[0] for row in cursor.fetchall()]
+    Label(top, text="Wybierz tabelę:").pack()
+    table_name_var = StringVar()
+    table_menu = OptionMenu(top, table_name_var, "", *tables)
+    table_menu.pack()
+    Button(top, text="Wybierz tabelę", command=select_table).pack()
+    entries={}
+
+def delete_record(root, cursor,conn):
+    ...
 
 def print_any_table(root, cursor):
     def select_table():
@@ -167,14 +234,17 @@ if __name__ == "__main__":
     bottom_frame.place(x=0,y=420)
     # Table_test
 
-    # button1 = Button(left_frame, text="Wyprintuj uczestników kursu",command=lambda: print_table(bottom_frame, cursor, "pracownicy"))
+    
     button1=Button(left_frame, text="Pokaż tabelę", command=lambda: print_any_table(root,cursor))
     button2 = Button(left_frame, text="Dodaj rekord", command=lambda: add_record(root, cursor, connection))
     button3 = Button(left_frame, text="Pokaż widok(td)", command=lambda: print_table(root,cursor,"zamowienie_szczegoly"))
     button4=Button(left_frame, text="Suma kosztow wszystkich zamowien", command=lambda: sum_costs(root,cursor))
-    # button1.place(x=50, y=25)
+    button5 = Button(left_frame, text="Edytuj rekord",command=lambda: edit_record(root, cursor, connection))
+    button6=Button(left_frame, text="Usuń rekord", command=lambda: delete_record(root, cursor, connection))
     button1.place(x=50,y=25)
     button2.place(x=50, y=75)
     button3.place(x=50,y=125)
     button4.place(x=50,y=175)
+    button5.place(x=50,y=225)
+    button6.place(x=50,y=275)
     root.mainloop()
